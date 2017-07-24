79b76a5498f29fabe4686c33014d2508e1e933f7
Chad Stein <chadbenstein@gmail.com>
2017-07-23 20:52:03 -0400

    Update practice4.py

--- practice4.py
+++ practice4.py
@@ -1,5 +1,6 @@
 #Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 
+# EXTRA CREDIT - use the "range" function
 
 #is 26 a divisor for itself? because 26 / 26 == 1
 # need to loop through function and have raw_input%i every time.
@@ -16,11 +17,20 @@ def divisor_check():
         i += 1 #increase the index by one, test again
     return new_list
 
-    
-
-
-
 print divisor_check()
 
+
+# Extra Credit
+
+def div_check():
+    user_number = int(raw_input("Pick a number. I'll tell you all divisors for it: ")) #variable to store user request
+    divisor_list = [] #empty list to send values
+    
+    for i in range(1, user_number+1): #start range at 1 because math cannot divide by 0. Add 1 to second argument to include the input from user as it divides into itself
+        if user_number%i==0: #if there is no remainder
+            divisor_list.append(i) #add that number to our empty list
+    return divisor_list #set definition value to this list
+
+print div_check()
 
 #your code here
